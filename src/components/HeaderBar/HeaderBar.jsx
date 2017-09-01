de135bf0ca812bf12d85ea76240a6061cda047da
René Füchtenkordt <renef132@gmail.com>
2017-09-01 22:56:59 +0200

    timerComponent initial layout

--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -10,11 +10,11 @@ const HeaderBar = props => (
     <div className='headerBar'>
         <div className='headerBar__wrapper'>
             <div className='headerBar__leftElement'><img src={props.logo} className='headerBar__logo'
-                                                         alt={props.titlePrimary + props.titleSecondary + ' logo'}/><span
+                                                         alt={props.title[0] + props.title[1] + ' logo'}/><span
                 className='headerBar__logoTextPrimary'
-                style={{color: props.muiTheme.palette.accent1Color}}>{props.titlePrimary}</span><span
+                style={{color: props.muiTheme.palette.accent1Color}}>{props.title[0]}</span><span
                 className='headerBar__logoTextSecondary'
-                style={{color: props.muiTheme.palette.accent1Color}}>{props.titleSecondary}</span></div>
+                style={{color: props.muiTheme.palette.accent1Color}}>{props.title[1]}</span></div>
             <div className='headerBar__rightElement'>
                 <RaisedButton label={props.buttonLabel} secondary={true} style={props.customButton.style} buttonStyle={props.customButton.buttonStyle}
                               overlayStyle={props.customButton.overlayStyle} className='headerBar_raisedButton'/>
