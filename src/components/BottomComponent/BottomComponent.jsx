fe6c56064b2f37f414e0c21ae9d5d98a390607c2
fabianlurz <fabian@eese.de>
2017-09-28 20:16:29 +0200

    cleanup

--- src/components/BottomComponent/BottomComponent.jsx
+++ src/components/BottomComponent/BottomComponent.jsx
@@ -19,51 +19,6 @@ const chartData = [
     { Time: '14:00', Prediction: 250 },
 ];
 
-const historyListData = [
-  {
-    address: '0x22b854DBF5c9A20f5C3374E814733060C942AeDf',
-    prediction: '$ 222.10',
-    timestamp: '1504524903805',
-    id: shortid.generate(),
-  },
-  {
-    address: '0xeB3a84E326DE0cF8976fDfB0231AD31Ed8f19f28',
-    prediction: '$ 227.50',
-    timestamp: '1504524908741',
-    id: shortid.generate(),
-  },
-  {
-    address: '0x22b854DBF5c9A20f5C3374E814733060C942AeDf',
-    prediction: '$ 229.70',
-    timestamp: '1504524913061',
-    id: shortid.generate(),
-  },
-  {
-    address: '0xFa705A686fe2d02D11cFc35fB5fEE40594ABD1B1',
-    prediction: '$ 212.30',
-    timestamp: '1504524916965',
-    id: shortid.generate(),
-  },
-  {
-    address: '0x35A1eea8AE6f734EfE14fc3715Ab51785D8D1D84',
-    prediction: '$ 217.90',
-    timestamp: '1504524920701',
-    id: shortid.generate(),
-  },
-  {
-    address: '0x22b854DBF5c9A20f5C3374E814733060C942AeDf',
-    prediction: '$ 218.90',
-    timestamp: '1504524924943',
-    id: shortid.generate(),
-  },
-  {
-    address: '0xf67757E7C326b5c1Bb8C0012B2644661011580E7',
-    prediction: '$ 205.00',
-    timestamp: '1504524929105',
-    id: shortid.generate(),
-  },
-];
-
 const BottomComponent = props =>
     (
       <div className="bottomComponent">
@@ -122,7 +77,7 @@ const BottomComponent = props =>
             <Row>
               <Col xs={12} md={12}>
                 <div className="bottomComponent__paddingWrapper">
-                  <HistoryList data={historyListData} />
+                  <HistoryList />
                 </div>
               </Col>
             </Row>
